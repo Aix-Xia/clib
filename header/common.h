6508da96763c183ea9f41b316c058e675dfb4ed5
夏季（Ji Xia） <E00131@icleague.com>
2024-04-29 14:02:15 +0800

    complete print debug; rename user.h to hlib.h

--- header/common.h
+++ header/common.h
@@ -1,9 +1,24 @@
 #pragma once
 #include <stdarg.h>
 #include <stdio.h>
+#include <stdlib.h>
+
+#define COMPLETE		0
+#define IMCOMPLETE		1
+#define ABNORMAL		2
+
+#define BUFSIZE		1024
 
 
+extern int DebugDisable();
+extern int DebugEnable();
 extern int DebugPrintf(const char* fmt, ...);
+
+extern int WarnDisable();
+extern int WarnEnable();
+extern int WarnPrintf(const char* fmt, ...);
+
+extern void ErrorPrintf(const char* fmt, ...);
 
 
 
---
+++ source/common.cpp
@@ -0,0 +1,58 @@
+#include "../hlib.h"
+
+
+bool s_flag_debug_print = true;
+bool s_flag_warn_print = true;
+
+int DebugDisable() {
+    s_flag_debug_print = false;
+    return COMPLETE;
+}
+int DebugEnable() {
+    s_flag_debug_print = true;
+    return COMPLETE;
+}
+int DebugPrintf(const char* fmt, ...) {
+    if (!s_flag_debug_print) {
+        return IMCOMPLETE;
+    }
+    char buffer[BUFSIZE];
+    va_list ap;
+    va_start(ap, fmt);
+    vsprintf(buffer, fmt, ap);
+    printf("[DEBUG]%s", buffer);
+    va_end(ap);
+    return COMPLETE;
+}
+
+int WarnDisable() {
+    s_flag_warn_print = false;
+    return COMPLETE;
+}
+int WarnEnable() {
+    s_flag_warn_print = true;
+    return COMPLETE;
+}
+int WarnPrintf(const char* fmt, ...) {
+    if (!s_flag_warn_print) {
+        return IMCOMPLETE;
+    }
+    char buffer[BUFSIZE];
+    va_list ap;
+    va_start(ap, fmt);
+    vsprintf(buffer, fmt, ap);
+    printf("[WARN]%s", buffer);
+    va_end(ap);
+    return COMPLETE;
+}
+
+void ErrorPrintf(const char* fmt, ...) {
+    char buffer[BUFSIZE];
+    va_list ap;
+    va_start(ap, fmt);
+    vsprintf(buffer, fmt, ap);
+    printf("[ERROR]%s", buffer);
+    va_end(ap);
+
+    exit(0);
+}
