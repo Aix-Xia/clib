5475ab023a2c425549c1a8eba6456cd1718ac4bd
夏季（Ji Xia） <E00131@icleague.com>
2024-05-09 16:00:52 +0800

    add macro.h

---
+++ header/macro.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#define _CONCAT_(a,b)		a##b
+#define CONCAT(a,b)			_CONCAT_(a,b)
--- mlib.h
+++ mlib.h
@@ -4,3 +4,4 @@
 #include "./header/bmp.h"
 #include "./header/mydirent.h"
 #include "./header/iterator.h"
+#include "./header/macro.h"
